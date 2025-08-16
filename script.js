dd312e2713cdce652e50e9ee554f87b1a7cec49b
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-08-16 23:45:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constractor(width,height){
+		this._width;
+		this._height;
+	}
+	get width(){
+		return this._width;
+	}
+	get heigth(){
+		return this.heigth;
+	}
 
-class Square extends Animal {}
+	getArea(){
+		return (this._heigth*this._width);
+	}
+}
+
+class Square extends Animal {
+	constractor(width,heigth){
+		super(width,heigth)
+	}
+	getPerimeter(){
+		return (4*this.width)
+	}
+}
+// Creating a Rectangle instance
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
+
+// Creating a Square instance
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
